896321c357376081fafccaf5400c79aede4601bc
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2023-09-10 18:46:58 -0400

    init commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.getElementById('container');
+
+fetch('data.json')
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(item => {
+            const listItem = document.createElement('div');
+            listItem.className = 'box'; 
+            listItem.innerHTML = `
+                <img src="${item.img}" alt="${item.name}">
+                <h4 style="color: #EBE114; margin-bottom: 40px;">${item.name}</h4>
+                <h4 style="color: white; margin-bottom: 20px;">${item.album}</h4>
+                <h4 style="color: grey !important; display: flex; align-items: flex-end;">${item.genre} ${"||"} ${item.price}</h4>
+            `;
+            container.appendChild(listItem);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+
