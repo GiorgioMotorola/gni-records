4d12f01829207b10018228ce4154a316bcf45b14
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2023-09-22 19:27:50 -0400

    prettier formatting

--- albumScript.js
+++ albumScript.js
@@ -1,82 +1,83 @@
 function displayRelatedAlbums(data, genre, currentId) {
-    const relatedAlbums = data.filter(item => item.genre === genre && parseInt(item.id) !== parseInt(currentId));
+  const relatedAlbums = data.filter(
+    (item) => item.genre === genre && parseInt(item.id) !== parseInt(currentId)
+  );
 
-    if (relatedAlbums.length > 0) {
-        const relatedAlbumsContainer = document.getElementById('related-albums');
-        relatedAlbumsContainer.innerHTML = '';
+  if (relatedAlbums.length > 0) {
+    const relatedAlbumsContainer = document.getElementById("related-albums");
+    relatedAlbumsContainer.innerHTML = "";
 
-        relatedAlbums.forEach(relatedAlbum => {
-            const albumCard = document.createElement('div');
-            albumCard.classList.add('album-card');
+    relatedAlbums.forEach((relatedAlbum) => {
+      const albumCard = document.createElement("div");
+      albumCard.classList.add("album-card");
 
-            const albumLink = document.createElement('a');
-            albumLink.href = `album.html?id=${relatedAlbum.id}`; 
-            albumLink.classList.add('album-link'); 
+      const albumLink = document.createElement("a");
+      albumLink.href = `album.html?id=${relatedAlbum.id}`;
+      albumLink.classList.add("album-link");
 
-            albumLink.addEventListener('click', (e) => {
-                e.preventDefault(); 
-                window.location.href = albumLink.href;
-            });
+      albumLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.location.href = albumLink.href;
+      });
 
-            const albumImage = document.createElement('img');
-            albumImage.src = relatedAlbum.img;
+      const albumImage = document.createElement("img");
+      albumImage.src = relatedAlbum.img;
 
-            const albumName = document.createElement('p');
-            albumName.textContent = relatedAlbum.album;
+      const albumName = document.createElement("p");
+      albumName.textContent = relatedAlbum.album;
 
-            const artistName = document.createElement('p');
-            artistName.textContent = relatedAlbum.name;
+      const artistName = document.createElement("p");
+      artistName.textContent = relatedAlbum.name;
 
-            albumLink.appendChild(albumImage);
-            albumLink.appendChild(albumName);
-            albumLink.appendChild(artistName);
+      albumLink.appendChild(albumImage);
+      albumLink.appendChild(albumName);
+      albumLink.appendChild(artistName);
 
-            albumCard.appendChild(albumLink);
+      albumCard.appendChild(albumLink);
 
-            relatedAlbumsContainer.appendChild(albumCard);
-        });
-    } else {
-        console.log('No related albums found for this genre.');
-    }
+      relatedAlbumsContainer.appendChild(albumCard);
+    });
+  } else {
+    console.log("No related albums found for this genre.");
+  }
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const id = urlParams.get("id");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
+  if (!id || isNaN(id)) {
+    console.error("Invalid or missing ID");
+  } else {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const item = data.find((item) => item.id === parseInt(id));
+        if (item) {
+          document.getElementById("album-image").src = item.img;
+          document.getElementById("artist-name").textContent = item.name;
+          document.getElementById("album-name").textContent = item.album;
+          document.getElementById(
+            "album-genre-price"
+          ).textContent = `${item.genre} || ${item.price}`;
+          document.getElementById("album-year").textContent = item.year;
 
-    if (!id || isNaN(id)) {
-        console.error('Invalid or missing ID');
-    } else {
-        fetch('data.json')
-            .then(res => res.json())
-            .then(data => {
-                const item = data.find(item => item.id === parseInt(id));
-                if (item) {
-                    document.getElementById('album-image').src = item.img;
-                    document.getElementById('artist-name').textContent = item.name;
-                    document.getElementById('album-name').textContent = item.album;
-                    document.getElementById('album-genre-price').textContent = `${item.genre} || ${item.price}`;
-                    document.getElementById('album-year').textContent = item.year;
+          const trackList = document.getElementById("track-list");
+          trackList.innerHTML = "";
 
-                    const trackList = document.getElementById('track-list');
-                    trackList.innerHTML = '';
+          item.tracks.forEach((track) => {
+            const li = document.createElement("li");
+            li.textContent = track;
+            trackList.appendChild(li);
+          });
 
-                    item.tracks.forEach((track) => {
-                        const li = document.createElement('li');
-                        li.textContent = track;
-                        trackList.appendChild(li);
-                    });
-
-                    displayRelatedAlbums(data, item.genre, id);
-
-
-                } else {
-                    console.error(`Album ${id} not found`);
-                }
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    }
+          displayRelatedAlbums(data, item.genre, id);
+        } else {
+          console.error(`Album ${id} not found`);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 });
--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-const container = document.getElementById('container');
+const container = document.getElementById("container");
 
-fetch('data.json')
-    .then(res => res.json())
-    .then(data => {
-        data.forEach(item => {
-            const listItem = document.createElement('div');
-            listItem.className = 'box'; 
-            listItem.innerHTML = `
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((item) => {
+      const listItem = document.createElement("div");
+      listItem.className = "box";
+      listItem.innerHTML = `
                 <img src="${item.img}" alt="${item.name}">
-                <h4 style="color: #000000; margin-bottom: 40px;">${item.name}</h4>
-                <h4 style="color: white; margin-bottom: 20px;">${item.album}</h4>
-                <h4 style="color: #E98074 !important; display: flex; align-items: flex-end;">${item.genre} ${"||"} ${item.price}</h4>
+                <h4 style="color: #000000; margin-bottom: 40px;">${
+                  item.name
+                }</h4>
+                <h4 style="color: white; margin-bottom: 20px;">${
+                  item.album
+                }</h4>
+                <h4 style="color: #E98074 !important; display: flex; align-items: flex-end;">${
+                  item.genre
+                } ${"||"} ${item.price}</h4>
             `;
 
-            listItem.addEventListener('click', () => {
-                window.location.href = `album.html?id=${item.id}`;
-            });
+      listItem.addEventListener("click", () => {
+        window.location.href = `album.html?id=${item.id}`;
+      });
 
-            container.appendChild(listItem);
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error);
+      container.appendChild(listItem);
     });
-
-    
-    
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
