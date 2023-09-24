24b0df30b447d6718198c7d0a52cefad511a9620
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2023-09-23 22:35:38 -0400

    new scroll bar, reorganized css and js files into own folder, added simple footer, added nav and footer css and html/js into their own folders, need to figure out wonky stuff going on with index footer

---
+++ js/navAndFooter.js
@@ -0,0 +1,40 @@
+function includeNavAndFooter() {
+  const nav = document.createElement("nav");
+  nav.innerHTML = `     
+    <nav id="navbar">
+    <div class="logo">
+      <img
+        class="icon"
+        src="./img/rekkid.png"
+        alt=""
+        style="background-color: #e85a4f"
+      />
+      GNI RECORDS
+    </div>
+    <ul class="nav-links">
+      <input type="checkbox" id="checkbox_toggle" />
+      <label
+        for="checkbox_toggle"
+        class="hamburger"
+        style="background-color: #e85a4f"
+        >&#9776;</label
+      >
+      <div class="menu">
+        <li><a href="/">Home</a></li>
+        <li><a href="/">Stock</a></li>
+        <li><a href="/">Stop By</a></li>
+      </div>
+    </ul>
+  </nav>
+      `;
+
+  const footer = document.createElement("footer");
+  footer.innerHTML = `       
+          <p>&copy; 2023 GNI Records.</p>
+      `;
+
+  document.body.insertBefore(nav, document.body.firstChild);
+  document.body.appendChild(footer);
+}
+
+includeNavAndFooter();
